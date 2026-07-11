76abf45601444555f5b9b2be7103d1cfb9654d9f
Glitch (elfin-ten-marble) <none>
2026-07-11 04:29:55 -0500

    comment out debug

--- app.js
+++ app.js
@@ -331,13 +331,17 @@ app.post("/", async (req, res) => {
         return;
     }
 
-    // working here
+    /* 
     console.log("=== LTI 1.1 DEBUG ===");
     console.log("Content-Type:", req.headers["content-type"]);
     console.log("Consumer Key:", req.body.oauth_consumer_key);
     console.log("Launch URL:", req.protocol + "://" + req.get("host") + req.originalUrl);
-    console.log("Body keys:", Object.keys(req.body));
-    // working here
+    console.log("Body keys:", Object.keys(req.body)); 
+    console.log("Host:", req.get("host"));
+    console.log("Protocol:", req.protocol);
+    console.log("Original URL:", req.originalUrl);
+    console.log("Secure:", req.secure);
+    */
 
     lmsData.valid_request(req, async (err, isValid) => {
         
